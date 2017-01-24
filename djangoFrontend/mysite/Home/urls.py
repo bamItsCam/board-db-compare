f985ecf53aa6fd855ae1b0c901cb6a3ebd023743
Abigail Liff <abigailliff@gmail.com>
2017-01-24 00:37:02 -0500

    Intial Django directory setup. Basic homepage container added using Bootstrap. Not sure what the author of this project wants the frontend to look like as of yet, but I wanted to get something started.

---
+++ djangoFrontend/mysite/Home/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url, include
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index') # The Home page shows up at the default URL (currently http:\\127.0.0.1:8000)
+]
